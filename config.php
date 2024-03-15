9e3e3f4294876a3278573f0aa7b2e8a6cf668ebf
Dedyshyn <134200030+Dedyshyn@users.noreply.github.com>
2024-03-15 20:38:38 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://www.wowonder.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: wowondersocial@gmail.com
+// +------------------------------------------------------------------------+
+// | WoWonder - The Ultimate PHP Social Networking Platform
+// | Copyright (c) 2016 WoWonder. All rights reserved.
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+$sql_db_host = "localhost";
+// MySQL Database User
+$sql_db_user = "root";
+// MySQL Database Password
+$sql_db_pass = "";
+// MySQL Database Name
+$sql_db_name = "social";
+
+// Site URL
+$site_url = "localhost"; // e.g (http://example.com)
+
+$auto_redirect = true;
+
+// Purchase code
+$purchase_code = ""; // Your purchase code, don't give it to anyone.
+
+$siteEncryptKey = "ae1f82c3ef3c7ccce4ad899051b98af2007648c4"; // Your site encrypt key, don't give it to anyone.
+?>
